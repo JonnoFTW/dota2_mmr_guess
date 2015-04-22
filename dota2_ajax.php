75256fd8083bb2eff3004dc5ad270c770acfb73e
JonnoFTW <jonmac1@gmail.com>
2015-04-22 03:17:08 -0400

    updated shit

--- dota2_ajax.php
+++ dota2_ajax.php
@@ -1,10 +1,18 @@
 <?php
 $id = $_GET['id'];
 if(!is_numeric($id)) {
-	echo json_encode(['error'=>'Please specify a valid steam id 64']);
+	echo json_encode(['error'=>'Please specify a valid id']);
 } else {
 	$key = file_get_contents('STEAM_KEY');
-	$json = json_decode(file_get_contents("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=JSON&key={$key}&account_id={$id}"));
-	// get all those matches
+	$json = json_decode(file_get_contents("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?format=JSON&key={$key}&account_id={$id}",true));
+	// get all those matches listed, should probably put them in a db for caching...
+	$mmr = 0;
+	$kills =0
+	foreach($json['result']['matches'] as $m) {
+		$matchId = $m['match_id'];
+		$match = json_decode(file_get_contents("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?format=JSON&key={$key}&match_id={$matchId}"));
+		// do some analysis
+		$match['result']
+	}
 	echo json_encode(['current-mmr'=>4,'skill-bracket'=>'good','most-matches'=>332,'total-matches'=>430]);
 }
